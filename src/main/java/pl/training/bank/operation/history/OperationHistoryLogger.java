9ccc075de622168595a50a659b2c620b56cc1c95
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-05-22 14:33:23 +0200

    Operation history

--- src/main/java/pl/training/bank/operation/history/OperationHistoryLogger.java
+++ src/main/java/pl/training/bank/operation/history/OperationHistoryLogger.java
@@ -2,8 +2,6 @@ package pl.training.bank.operation.history;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
 import pl.training.bank.account.Account;
 import pl.training.bank.account.AccountNotFoundException;
 import pl.training.bank.account.AccountRepository;
@@ -11,7 +9,6 @@ import pl.training.bank.disposition.Disposition;
 
 import java.util.Date;
 
-@Aspect
 @RequiredArgsConstructor
 public class OperationHistoryLogger {
 
@@ -20,16 +17,24 @@ public class OperationHistoryLogger {
     @NonNull
     private AccountRepository accountRepository;
 
-    @AfterReturning("execution(void pl.training.bank.disposition.DispositionService.process(..)) && args(disposition)")
     public void logOperation(Disposition disposition) {
-        Account account = accountRepository.getByNumber(disposition.getAccountNumber())
+        Account account = getAccount(disposition.getAccountNumber());
+        OperationHistoryEntry historyEntry = createOperationHistoryEntry(disposition, account);
+        operationHistoryRepository.save(historyEntry);
+    }
+
+    private Account getAccount(String accountNumber) {
+        return accountRepository.getByNumber(accountNumber)
                 .orElseThrow(AccountNotFoundException::new);
-        OperationHistoryEntry entry = new OperationHistoryEntry();
-        entry.setAccount(account);
-        entry.setDate(new Date());
-        entry.setFunds(disposition.getFunds());
-        entry.setType(disposition.getOperationName());
-        operationHistoryRepository.save(entry);
+    }
+
+    private OperationHistoryEntry createOperationHistoryEntry(Disposition disposition, Account account) {
+        OperationHistoryEntry historyEntry = new OperationHistoryEntry();
+        historyEntry.setAccount(account);
+        historyEntry.setDate(new Date());
+        historyEntry.setFunds(disposition.getFunds());
+        historyEntry.setType(disposition.getOperationName());
+        return historyEntry;
     }
 
 }
