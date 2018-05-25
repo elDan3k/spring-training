564339326ec24279b5297bdf579e4384e5f8d666
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-05-25 10:27:05 +0200

    Security

--- angular/src/app/bank/api.ts
+++ angular/src/app/bank/api.ts
@@ -2,11 +2,11 @@ import {environment} from "../../environments/environment";
 
 export class Api {
 
-  accounts = `${environment.baseUrl}/accounts`;
-  dispositions = `${environment.baseUrl}/dispositions`;
+  accounts = `${environment.baseUrl}${environment.apiVersion}/accounts`;
+  dispositions = `${environment.baseUrl}${environment.apiVersion}/dispositions`;
 
   oauthServer = `${environment.baseUrl}/oauth/token`;
-  users = `${environment.baseUrl}/users`;
-  activeUser = `${this.users}/active`;
+  users = `${environment.baseUrl}${environment.apiVersion}/users`;
+  activeUser = `${this.users}${environment.apiVersion}/active`;
 
 }
--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:8080/api/v1/'
+  baseUrl: 'http://localhost:8080',
+  apiVersion: '/api/v1'
 };
