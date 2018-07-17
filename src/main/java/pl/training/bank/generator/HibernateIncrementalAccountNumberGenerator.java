3dde304707647be7d00a42f57a29c7999d95ffe1
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-07-17 13:26:46 +0200

    Hibernate

--- src/main/java/pl/training/bank/generator/HibernateIncrementalAccountNumberGenerator.java
+++ src/main/java/pl/training/bank/generator/HibernateIncrementalAccountNumberGenerator.java
@@ -1,22 +1,23 @@
 package pl.training.bank.generator;
 
 import org.hibernate.SessionFactory;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.sql.DataSource;
 import java.util.concurrent.atomic.AtomicLong;
+
+import static java.lang.Long.parseLong;
 
 public class HibernateIncrementalAccountNumberGenerator extends IncrementalAccountNumberGenerator {
 
     private static final String SELECT_LAST_ACCOUNT_NUMBER = "select max(a.number) from Account a";
 
     public HibernateIncrementalAccountNumberGenerator(SessionFactory sessionFactory) {
-        String lastAccountNumber = sessionFactory.openSession()
-                .createQuery(SELECT_LAST_ACCOUNT_NUMBER, String.class)
-                .uniqueResult();
-        if (lastAccountNumber != null) {
-            setCounter(new AtomicLong(Long.parseLong(lastAccountNumber)));
-        }
+        sessionFactory.openSession().createQuery(SELECT_LAST_ACCOUNT_NUMBER, String.class)
+                .uniqueResultOptional()
+                .ifPresent(this::updateCounter);
+    }
+
+    private void updateCounter(String lastAccountNumber) {
+        setCounter(new AtomicLong(parseLong(lastAccountNumber)));
     }
 
 }
