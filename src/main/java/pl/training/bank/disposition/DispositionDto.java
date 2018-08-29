4a089b47593d7a1f6a180e3e2bff05c4fcbbbd75
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-08-29 10:56:47 +0200

    Ex

--- src/main/java/pl/training/bank/disposition/DispositionDto.java
+++ src/main/java/pl/training/bank/disposition/DispositionDto.java
@@ -2,13 +2,17 @@ package pl.training.bank.disposition;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "disposition")
 @Data
 public class DispositionDto {
 
+    @Pattern(regexp = "\\d{26}")
     private String accountNumber;
+    @Min(1)
     private long funds;
     private String operationName;
 
